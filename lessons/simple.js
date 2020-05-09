785e7d03d1e84030ec646d435457f7f8f1f90d49
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-09 19:30:18 -0400

    finish first module

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,17 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require("../math");
 
-let result, expected
+let result, expected;
 
-result = sum(3, 7)
-expected = 10
+// testing in JS is not that crazy! it's just does it throw an error when you expect something different!
+
+result = sum(3, 7);
+expected = 10;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 3)
-expected = 4
+result = subtract(7, 3);
+expected = 4;
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
